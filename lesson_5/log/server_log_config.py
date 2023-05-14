350f51b40b037a6c6c64163158bcc7d32e031b28
Troover <94111543+Zoobzick@users.noreply.github.com>
2023-05-14 20:28:22 +0300

    lesson_5 done (#5)

---
+++ lesson_5/log/server_log_config.py
@@ -0,0 +1,27 @@
+import logging.handlers
+import os
+import sys
+
+sys.path.append('../')
+
+SERVER_FORMATTER = logging.Formatter('%(asctime)s %(levelname)s %(filename)s %(message)s')
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+PATH = os.path.join(PATH, 'server.log')
+
+STREAM_HANDLER = logging.StreamHandler(sys.stderr)
+STREAM_HANDLER.setFormatter(SERVER_FORMATTER)
+STREAM_HANDLER.setLevel(logging.ERROR)
+LOG_FILE = logging.handlers.TimedRotatingFileHandler(PATH, encoding='utf8', interval=1, when='D')
+LOG_FILE.setFormatter(SERVER_FORMATTER)
+
+LOGGER = logging.getLogger('server')
+LOGGER.addHandler(STREAM_HANDLER)
+LOGGER.addHandler(LOG_FILE)
+LOGGER.setLevel(logging.DEBUG)
+
+if __name__ == '__main__':
+    LOGGER.critical('Critical Error')
+    LOGGER.error('Error')
+    LOGGER.debug('Debug INFO')
+    LOGGER.info('INFO message')
